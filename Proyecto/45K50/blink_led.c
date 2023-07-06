40c1d2db71578483a9a942b71348702fb34450f8
vhperaltaj <victor.peralta@microside.com>
2023-07-06 15:48:40 -0600

    Change encoding to UTF-8

--- Proyecto/45K50/blink_led.c
+++ Proyecto/45K50/blink_led.c
@@ -11,14 +11,14 @@ Driver Version    :  1.0
 ********************************************************************************/
 
 /*******************************************************************************
-Para usar el código con bootloader, configurar como lo indica MICROSIDE:
+Para usar el cÃ³digo con bootloader, configurar como lo indica MICROSIDE:
 1) Tools> Options> Output> Output Settings> Marcar "Long HEX format"
 2) Project> Edit Project> Insertar 48.000000 en "MCU Clock Frequency [MHz]" box
 ********************************************************************************/
 
 #pragma orgall 0x1FFF           //Espacio reservado para bootloader
 
-#define LED LATA4_bit           //Se crea una definición donde se almacena el registro del pin
+#define LED LATA4_bit           //Se crea una definiciÃ³n donde se almacena el registro del pin
                                 //Tome en cuenta que en versiones anteriores se hacia uso del pin A1
 
 void main() org 0x2000          //Se especifica el segmento a partir de cual se almacenara el programa ya que existe un espacio
